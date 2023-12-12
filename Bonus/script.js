e0637071e769bd102fd115b547b0c35dc4339107
Giuseppe Riccardo Pugliese <giuseppe.riccardo.pugliese@gmail.com>
2023-12-12 15:44:24 +0100

    Setup: Bonus base scaffolding

---
+++ Bonus/script.js
@@ -0,0 +1,45 @@
+/*
+MILESTONE 1
+Stampare all’interno di una lista HTML un item per ogni todo.
+Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+
+MILESTONE 2
+Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+
+MILESTONE 3
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+*/
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: "",
+            todos: [
+                {
+                    text: 'Fare i compiti',
+                    done: false
+                },
+                {
+                    text: 'Fare la spesa',
+                    done: true
+                },
+                {
+                    text: 'Fare il bucato',
+                    done: false
+                }
+            ]
+        }
+    },
+    methods: {
+        delTask(i) { //MILESTONE 2
+            //Partendo dall'indice i gli dico di rimuovere il primo 
+            this.todos.splice(i, 1);
+        },
+        addTask() { //MILESTONE 3
+            this.todos.unshift({ text: this.message });
+            this.message = "";
+            //unshift = push pero' all'inizio anziche' alla fine
+        }
+    }
+}).mount("#app");
